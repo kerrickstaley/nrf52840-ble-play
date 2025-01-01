c86f2b09d0649e021fa0512912faf309c3adf268
Kerrick Staley <k@kerrickstaley.com>
2025-01-01 13:51:29 +0100

    Also install pyelftools

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ python3 -m virtualenv .west_venv
 (
     source .west_venv/bin/activate
     pip install west==1.3.0
+    # Not sure why West doesn't list this as a dependency, but we get a build
+    # error without it.
+    pip install pyelftools==0.31
 )
 .west_venv/bin/west init
 .west_venv/bin/west update
